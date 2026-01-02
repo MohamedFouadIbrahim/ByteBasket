09f2a13209da96cedfc2529ec05388041f0891d6
Mohamed Fouad <mohamedfouad@Mohameds-MacBook-Pro.local>
2026-01-02 15:16:04 +0400

    add redux globally in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import { RootNavigator } from '@/navigation';
+import createStore from "@/redux/store";
 import { NavigationContainer } from '@react-navigation/native';
 import * as React from 'react';
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
+const { store, persistor } = createStore();
 
 export default function App() {
   return (
-    <NavigationContainer>
-        <RootNavigator />
-    </NavigationContainer>
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            <NavigationContainer>
+                <RootNavigator />
+            </NavigationContainer>
+        </PersistGate>
+    </Provider>
   );
 }
